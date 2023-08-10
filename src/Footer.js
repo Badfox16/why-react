4bcff57b8a700b1dc115d167554c4203b2afa196
¨badfox16¨ <kingbadfox3@gmail.com>
2023-08-10 02:01:48 +0200

    Added a footer and some extra css

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <p> Copyrights 2023 "Last name here" development. All rights reserved.</p>
+        </div>
+    );
+}
+
+export default Footer;
