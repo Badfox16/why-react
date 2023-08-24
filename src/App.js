665193734aeb298c0938a1533de819ba9c785ac8
badfox16 <kingbadfox3@gmail.com>
2023-08-24 21:27:07 +0200

    alguns updates basicos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './Header';
 
 function App() {
   return (
-  <div>
-      <div><header className='head'>      
-      <img src={logo} className="App-logo" alt="logo" />
-  </header>
-  </div>
-    <div className="App">
-      
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first releasead in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>It's mantained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+    <div>
 
-    </div>
+      <Header />
+
+      <div className="App">
+
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Was first releasead in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>It's mantained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+
+      </div>
     </div>
   );
 }
